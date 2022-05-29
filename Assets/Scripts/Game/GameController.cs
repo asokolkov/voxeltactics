cd28b4854153af2034391dac121248beb58f37c7
Aleksey <a.e.sokolkov@gmail.com>
2022-05-29 19:47:20 +0500

    Ultimate OnClick event listener

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -12,32 +12,72 @@ public class GameController : MonoBehaviour
     public Player Enemy;
     public Player ActivePlayer;
 
-    public GameObject fieldGO;
-    private Field field;
+    public GameObject allyBoardGO;
+    public GameObject enemyBoardGO;
+    
+    [NonSerialized] public Board AllyBoard;
+    [NonSerialized] public Board EnemyBoard;
+    
+    [NonSerialized] public Piece SelectedPiece;
+    
+    private const float SquareSize = 1;
+    private const int BoardSize = 3;
     
     private void Awake()
     {
         SetDependencies();
-        CreatePlayers();
+        Ally = new Player(AllyBoard);
+        Enemy = new Player(EnemyBoard);
+    }
+    
+    private void Start()
+    {
+        ActivePlayer = Ally;
+        InitializePiece(new Vector2Int(0, 0), EnemyBoard, "Penguin");
+        GetAllPossiblePlayerBoardInteractions(ActivePlayer);
     }
     
     private void SetDependencies()
     {
         pieceCreator = GetComponent<PieceCreator>();
-        field = fieldGO.GetComponent<Field>();
+        AllyBoard = allyBoardGO.GetComponent<Board>();
+        EnemyBoard = enemyBoardGO.GetComponent<Board>();
     }
 
-    private void CreatePlayers()
+    public void OnClick(Vector3 inputPosition, GameObject selectedObject)
     {
-        Ally = new Player(field.AllyBoard);
-        Enemy = new Player(field.EnemyBoard);
+        var board = selectedObject.GetComponent<Board>();
+        var coords = board.CalculateCoords(inputPosition);
+        var piece = board.GetPieceOnSquare(coords);
+        if (SelectedPiece)
+        {
+            if (piece != null && SelectedPiece == piece) 
+                DeselectPiece();
+            else if (piece != null && SelectedPiece != piece)
+                SelectPiece(piece);
+            else
+                MovePiece(coords, SelectedPiece);
+        }
+        else if (piece != null) SelectPiece(piece);
     }
-
-    private void Start()
+    
+    private void MovePiece(Vector2Int coords, Piece piece)
     {
-        InitializePiece(new Vector2Int(0, 0), field.EnemyBoard, "Penguin");
-        ActivePlayer = Ally;
-        GetAllPossiblePlayerBoardInteractions(ActivePlayer);
+        piece.Board.UpdateBoardOnPieceMove(coords, piece.Position, 
+            piece, null);
+        SelectedPiece.MovePiece(coords);
+        DeselectPiece();
+    }
+    
+    private void SelectPiece(Piece piece)
+    {
+        SelectedPiece = piece;
+        piece.transform.position += new Vector3(0, 0.1f, 0);
+    }
+    
+    private void DeselectPiece()
+    {
+        SelectedPiece = null;
     }
 
     private void GetAllPossiblePlayerBoardInteractions(Player player)
---
+++ Assets/Scripts/InputSystem/InputHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(GameController))]
+public class InputHandler : MonoBehaviour, IInputHandler
+{
+    private GameController gameController;
+
+    private void Awake()
+    {
+        gameController = GetComponent<GameController>();
+    }
+    
+    public void ProcessInput(Vector3 inputPosition, GameObject selectedObject, Action callback)
+    {
+        gameController.OnClick(inputPosition, selectedObject);
+    }
+}
